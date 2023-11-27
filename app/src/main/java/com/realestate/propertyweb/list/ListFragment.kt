8847c8f35d0a44eedf9b3550e3275a37c55ad401
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2023-11-28 00:44:51 +0100

    Project setup with empty List Fragment

---
+++ app/src/main/java/com/realestate/propertyweb/list/ListFragment.kt
@@ -0,0 +1,38 @@
+package com.realestate.propertyweb.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(color = Color.White)
+                        .padding(16.dp)
+                ) {
+                    Text(text = "ListFragment")
+                }
+            }
+        }
+    }
+}
