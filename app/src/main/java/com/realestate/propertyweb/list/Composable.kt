590c19b6f21b69bd3b795cba9f9a8b462a3b0dcc
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2023-11-30 00:25:27 +0100

    Update property listing screen

--- app/src/main/java/com/realestate/propertyweb/list/Composable.kt
+++ app/src/main/java/com/realestate/propertyweb/list/Composable.kt
@@ -3,14 +3,11 @@ package com.realestate.propertyweb.list
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
@@ -21,9 +18,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import coil.request.ImageRequest
 
 @Composable
 internal fun PropertyListScreen(state: ListViewModel.UIState) {
@@ -59,9 +58,11 @@ internal fun PropertyListScreen(state: ListViewModel.UIState) {
 
 @Composable
 fun PropertyList(properties: List<Property>) {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
         properties.forEach { property ->
             PropertyItem(property = property)
             Spacer(modifier = Modifier.height(16.dp))
@@ -71,42 +72,49 @@ fun PropertyList(properties: List<Property>) {
 
 @Composable
 fun PropertyItem(property: Property) {
-    Card {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-        ) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Column(modifier = Modifier.padding(bottom = 16.dp)) {
             AsyncImage(
-                model = property.url,
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(property.url)
+                    .crossfade(true)
+                    .build(),
                 contentDescription = null,
                 modifier = Modifier
-                    .padding(16.dp)
-                    .width(150.dp)
-                    .height(150.dp),
+                    .fillMaxWidth()
+                    .height(200.dp),
             )
-            Spacer(modifier = Modifier.width(16.dp))
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxHeight()
-            ) {
-                Text(
-                    text = property.propertyType,
-                    modifier = Modifier.fillMaxWidth(),
-                    style = MaterialTheme.typography.headlineLarge
-                )
-                Text(
-                    text = property.city,
-                    modifier = Modifier.fillMaxWidth(),
-                    style = MaterialTheme.typography.bodyLarge
-                )
-                Text(
-                    text = "${property.price} sq.m",
-                    modifier = Modifier.fillMaxWidth(),
-                    style = MaterialTheme.typography.bodySmall
-                )
-            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = property.propertyType,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                style = MaterialTheme.typography.headlineSmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = property.city,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                style = MaterialTheme.typography.bodyMedium
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "${property.price} sq.m",
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = property.professional,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                style = MaterialTheme.typography.bodyLarge
+            )
         }
     }
 }
