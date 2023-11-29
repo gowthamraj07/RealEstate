429f9945060dd1e56988b3cd3130b06af34b0342
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2023-11-29 14:08:16 +0100

    Show properties in list when state emits list of properties

--- app/src/androidTest/java/com/realestate/propertyweb/list/PropertyListScreenShotTest.kt
+++ app/src/androidTest/java/com/realestate/propertyweb/list/PropertyListScreenShotTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 
 
 // ./gradlew executeScreenshotTests -Pandroid.testInstrumentationRunnerArguments.class=com.realestate.propertyweb.list.PropertyListScreenShotTest -Precord
-class PropertyListScreenShotTest: ScreenshotTest {
+class PropertyListScreenShotTest : ScreenshotTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
@@ -28,6 +28,34 @@ class PropertyListScreenShotTest: ScreenshotTest {
         with(composeTestRule) {
             setContent {
                 PropertyListScreen(state = ListViewModel.UIState.Error(Exception("Something went wrong")))
+            }
+
+            compareScreenshot(this)
+        }
+    }
+
+    @Test
+    fun propertyListState() {
+        with(composeTestRule) {
+            setContent {
+                PropertyListScreen(
+                    state = ListViewModel.UIState.Content(
+                        properties = listOf(
+                            Property(
+                                bedrooms = 3,
+                                city = "London",
+                                id = 1,
+                                area = 100,
+                                url = "https://v.seloger.com/s/crop/590x330/visuels/1/7/t/3/17t3fitclms3bzwv8qshbyzh9dw32e9l0p0udr80k.jpg",
+                                price = 100000,
+                                professional = "John Doe",
+                                propertyType = "House",
+                                offerType = 1,
+                                rooms = 5
+                            )
+                        )
+                    )
+                )
             }
 
             compareScreenshot(this)
--- app/src/main/java/com/realestate/propertyweb/list/Composable.kt
+++ app/src/main/java/com/realestate/propertyweb/list/Composable.kt
@@ -3,15 +3,25 @@ package com.realestate.propertyweb.list
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 internal fun PropertyListScreen(state: ListViewModel.UIState) {
@@ -23,7 +33,6 @@ internal fun PropertyListScreen(state: ListViewModel.UIState) {
     ) {
         when (state) {
             is ListViewModel.UIState.Loading -> {
-                // show circular progress bar
                 Box (modifier = Modifier.fillMaxSize()) {
                     CircularProgressIndicator(modifier = Modifier.align(alignment = Alignment.Center))
                 }
@@ -36,8 +45,53 @@ internal fun PropertyListScreen(state: ListViewModel.UIState) {
             }
 
             is ListViewModel.UIState.Content -> {
-
+                PropertyList(properties = state.properties)
             }
         }
     }
+}
+
+@Composable
+fun PropertyList(properties: List<Property>) {
+    Column {
+        properties.forEach { property ->
+            PropertyItem(property = property)
+        }
+    }
+}
+
+@Composable
+fun PropertyItem(property: Property) {
+    Card {
+        Row (modifier = Modifier.fillMaxWidth().height(200.dp)) {
+            AsyncImage(
+                model = property.url,
+                contentDescription = null,
+                modifier = Modifier.padding(16.dp).width(150.dp).height(150.dp),
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(modifier = Modifier.padding(16.dp).fillMaxHeight()) {
+                Text(text = property.propertyType, modifier = Modifier.fillMaxWidth(), style = MaterialTheme.typography.headlineLarge)
+                Text(text = property.city, modifier = Modifier.fillMaxWidth(), style = MaterialTheme.typography.bodyLarge)
+                Text(text = "${property.price} sq.m", modifier = Modifier.fillMaxWidth(), style = MaterialTheme.typography.bodySmall)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PropertyItemPreview() {
+    PropertyItem(property = Property(
+        bedrooms = 3,
+        city = "London",
+        id = 1,
+        area = 100,
+        url = "https://v.seloger.com/s/crop/590x330/visuels/1/7/t/3/17t3fitclms3bzwv8qshbyzh9dw32e9l0p0udr80k.jpg",
+        price = 100000,
+        professional = "John Doe",
+        propertyType = "House",
+        offerType = 1,
+        rooms = 5
+    ))
 }
