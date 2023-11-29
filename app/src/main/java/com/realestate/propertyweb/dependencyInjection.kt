f4feb38fe263e1c4c78d70e2c8d2e5a1c2adef64
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2023-11-29 01:04:26 +0100

    Configure dependencyInjection and Application name in AndroidManifest.xml for Koin to inject dependencies properly

--- app/src/main/java/com/realestate/propertyweb/dependencyInjection.kt
+++ app/src/main/java/com/realestate/propertyweb/dependencyInjection.kt
@@ -1,5 +1,7 @@
+import com.realestate.propertyweb.list.ListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-
+    viewModel { ListViewModel() }
 }
