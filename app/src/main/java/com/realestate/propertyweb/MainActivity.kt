5b843353d465456c34e668228f7a456b61c71077
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2023-11-30 01:27:46 +0100

    Setup toolbar with navigation graph

--- app/src/main/java/com/realestate/propertyweb/MainActivity.kt
+++ app/src/main/java/com/realestate/propertyweb/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.realestate.propertyweb
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_main_activity)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
 }
